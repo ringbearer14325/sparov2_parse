52bc97db9bb4188af628e507a6cdc7234b5802f4
ringbearer-ramos14325 <61596740+ringbearer-ramos14325@users.noreply.github.com>
2020-04-29 14:11:44 -0400

    Add files via upload

---
+++ UI.js
@@ -0,0 +1,34 @@
+
+
+ChatPanel
+MessagePanel
+MessageMode
+CallVideo/VoicePanel
+SearchBar
+
+
+[
+    {name: "Message", id: "user", options: "message options", panelActive: "true"}
+
+];
+
+[
+    {messageMode: "", notificationNumberIndicator: "0" }
+
+];
+
+[ 
+    {panelOptions: "call(video/voice)", newMessage: "true, false" }
+
+];
+
+[ 
+    { searchBar }
+    
+];
+
+
+[
+    {videoBar: "videoTime", "VideoTools", "UserVideo", panel: "true/false" }  
+    
+];
