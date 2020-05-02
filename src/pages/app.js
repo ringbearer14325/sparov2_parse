1771bb6f0025604bf2b70080ce7a7b0ef539f294
Daniel <danielramos1981@gmail.com>
2020-05-01 21:32:22 -0400

    commit

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Parse from 'parse';
-=======
 import Parse from 'src/helpers/helper.php'; 
->>>>>>> 8345f6072ed48d1a6f7fcec7503ee4eabae82497
 import {
     Route,
     browserRouter as Router,
@@ -14,11 +11,7 @@ import {
     import Chat from './pages/Chat';
     import Signup from './pages/Signup';
     import Login from './pages/Login';
-<<<<<<< HEAD
-    import './css/styles.css';
-=======
     import '.src/css/styles.css';
->>>>>>> 8345f6072ed48d1a6f7fcec7503ee4eabae82497
 
 
 
