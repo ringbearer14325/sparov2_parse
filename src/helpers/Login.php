8ed57574e35e1b2bba5e02cd9e3ba2922ac37dfe
Daniel <danielramos1981@gmail.com>
2020-05-08 16:31:45 -0400

    ommit files. Login.php and load.php.

--- src/helpers/Login.php
+++ src/helpers/Login.php
@@ -13,11 +13,31 @@ public function __construct() {
   session_start();
 
   $this->db = $db;
-
+  
 }
 
 public function verify_login($post) {
+  if ( ! isset($post['username'] ) || ! isset($post['password'] ) ) {
+    return false;
+  }
 
+  //check if the user exists 
+  $user = $this->user_exists( $post['username'] );
+
+  if ( false !== $user ) {
+    if ( password_verify( $post['password'], $user->password ) ) {
+      $_SESSION['username'] = $user->username;
+
+      return true;
+  }
+}
+
+return false;
+
+}
+
+  
+  $user = ParseUser::logIn("myname", "mypass");
 
 
 }
@@ -43,7 +63,6 @@ if (isset($_GET[$applicationId]) && isset($_GET([$REST_API_KEY]))) {
 
 
 try {
-    $user = ParseUser::logIn("myname", "mypass");
     // Do stuff after successful login.
   } catch (ParseException $error) {
     // The login failed. Check error to see why.
---
+++ src/helpers/load.php
@@ -0,0 +1,9 @@
+<?php
+require_once('confog.php');
+require_once('includes/class-db.php');
+require_once('includes/class-login.php');
+
+
+?>
+
+
