718a2d1177636c3100afe64e02d6098d9d67eecd
Daniel <danielramos1981@gmail.com>
2020-05-08 12:41:59 -0400

    commit file. Keys for Parse server app REST and app Id.

---
+++ src/components/keys.php
@@ -0,0 +1,9 @@
+<?php
+
+$applicationId = "X-Parse-Application-Id: BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f";
+$REST_API_KEY = "X-Parse-REST-API-Key: swrFFIXJlFudtF3HkZPtfybDFRTmS7sPwvGUzQ9w";
+
+?>
+
+
+
