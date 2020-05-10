69afd3ecb9fe16bdb60aadd9862ade717e5cd75e
Daniel <danielramos1981@gmail.com>
2020-05-10 19:41:38 -0400

    ommit keys.php and login.json

--- src/components/keys.php
+++ src/components/keys.php
@@ -2,6 +2,33 @@
 $applicationId = "X-Parse-Application-Id: BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f";
 $REST_API_KEY = "X-Parse-REST-API-Key: swrFFIXJlFudtF3HkZPtfybDFRTmS7sPwvGUzQ9w";
 
+
+class one() {
+
+
+public function sendHttpRequestUser($url, $keys, $body) {
+  $url = header("https://parseapi.back4app.com/user");
+
+  $key = $_POST("X-Parse-Application-Id: BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f",
+"X-Parse-REST-API-Key: swrFFIXJlFudtF3HkZPtfybDFRTmS7sPwvGUzQ9w");
+$body = "src\components\user.json";
+
+}
+
+public function sendHttpRequestLogin($url, $keys, $body) {
+  if ($_GET(header($url)) && 
+
+  $key = $_POST("X-Parse-Application-Id: BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f",
+"X-Parse-REST-API-Key: swrFFIXJlFudtF3HkZPtfybDFRTmS7sPwvGUzQ9w");
+$body = "src\components\login.json";
+
+}
+sendHttpRequestUser(header("https://parseapi.back4app.com/user"), $_POST(
+        "X-Parse-Application-Id: BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f",
+        "X-Parse-REST-API-Key: swrFFIXJlFudtF3HkZPtfybDFRTmS7sPwvGUzQ9w"
+), "src\components\login.json" );
+
+}
 ?>
 
 
