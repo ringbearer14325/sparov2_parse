e6918dd003843eb75f01dee8c8e3bc2c331078f7
Daniel <danielramos1981@gmail.com>
2020-05-18 11:17:10 -0400

    Commit file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,38 +1,13 @@
 import React, { Component } from 'react';
-//import Header from '../components/Header';
-//import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
-//import "src/helpers/helper.php";
+
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            result: ''
-        };
-
-        // let install = new Parse.Installation();
-        // install.set("deviceType", navigator.userAgent);
-        // install.save().then((resp) => {
-        //     console.log('Created install object', resp);
-
-        //     this.setState({
-        //         result: 'New object created with objectId: ' + resp.id
-        //    })
-        // }, err => {
-        //     console.log('Error creating install object', err);
-
-        //     this.setState({
-        //         result: 'Failed to create new object, with error code: ' + err.message
-        //     })
-       // })
-    }    
+    
 
   render() {
     return (
       <div className="home">
-      {/* <Header></Header>  */}
         <section>
             <h1>{this.state.result}</h1>
           <div className="jumbotron jumbotron-fluid py-5">
@@ -46,7 +21,6 @@ export default class HomePage extends Component {
             </div>
           </div>
         </section>
-        {/* <Footer></Footer> */}
       </div>
     )
   }
