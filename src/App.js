877a9aca529ba4fef701a09a1860fde59763d288
Daniel <danielramos1981@gmail.com>
2020-05-21 17:16:24 -0400

    Commit files. Added app.js, index.js. Those were in the gitignore file.

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Home from "./pages/home";
+import Chat from "./pages/chat";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Parse from "./helpers/helper.php";
+import './css/style.css';
+
+
+
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+            <Redirect
+              to={{ pathname: "/login", state: { from: props.location } }}
+            />
+          )
+      }
+    />
+  );
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated === false ? (
+          <Component {...props} />
+        ) : (
+            <Redirect to="/chat" />
+          )
+      }
+    />
+  );
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    // add app and js keys for Parse Server
+    Parse.initialize("yzRv0M18nfehYtjg4toFBzWkRL1WL2Vs6CJVDISv", "4wPYRKbpTJeCdmFNaS31AiQZ8344aaYubk6Uo8VW");
+    Parse.ServerURL = 'https://parseapi.back4app.com';
+
+
+    this.state = {
+    //  authenticated: false,
+      loading: true,
+    };
+  }
+
+
+   componentDidMount() {
+      if (user) {
+        this.setState({
+         // authenticated: true,
+          loading: false
+        });
+      } else {
+        this.setState({
+        //  authenticated: false,
+          loading: false
+        });
+      }
+    };
+   
+  
+
+  render() {
+    return this.state.loading === true ? (
+      <div className="spinner-border text-success" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    ) : (
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <PrivateRoute
+              path="/chat"
+              authenticated={this.state.authenticated}
+              component={Chat}
+            />
+            <PublicRoute
+              path="/signup"
+              authenticated={this.state.authenticated}
+              component={Signup}
+            />
+            <PublicRoute
+              path="/login"
+              authenticated={this.state.authenticated}
+              component={Login}
+            />
+          </Switch>
+        </Router>
+      );
+  }
+}
+
+
+export default App;
