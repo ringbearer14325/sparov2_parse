56b05098b22cd9000835e2e48bd78d45e8e02de4
Daniel <danielramos1981@gmail.com>
2020-05-30 14:56:22 -0400

    Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Parse from 'Parse';
+
+
+
+
+<script src="node_modules/parse/dist/parse.min.js"></script>
+
+<script>
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+Parse.initialize(
+  'BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f', // This is your Application ID
+  '4wPYRKbpTJeCdmFNaS31AiQZ8344aaYubk6Uo8VW' // This is your Javascript key
+);
+</script>
