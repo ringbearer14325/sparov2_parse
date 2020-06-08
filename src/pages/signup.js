07646d9a53b88834a3a82a6fb760ad7ac84633af
Daniel <danielramos1981@gmail.com>
2020-06-08 11:20:57 -0400

    Commit files.

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -25,6 +25,29 @@ handleChange(event) {
 }
 
 async handleSubmit(event) {
+
+    var CommentBlock = React.createClass({
+  mixins: [ParseReact.Mixin], // Enable query subscriptions
+
+  observe: function() {
+    // Subscribe to all Comment objects, ordered by creation date
+    // The results will be available at this.data.comments
+    return {
+      comments: (new Parse.Query('Comment')).ascending('createdAt')
+    };
+  },
+
+  render: function() {
+    // Render the text of each comment as a list item
+    return (
+      <ul>
+        {this.data.comments.map(function(c) {
+          return <li>{c.text}</li>;
+        })}
+      </ul>
+    );
+  }
+});
     event.preventDefault();
     this.setState({ error: '' });
     try {
